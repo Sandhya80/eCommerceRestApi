114ca3e9488a8adb41bee57f46a6a4b0de90010a
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2023-11-24 23:31:55 +0000

    Update index.js (Modify middleware log message)

--- server/index.js
+++ server/index.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is now listening on Port ${port}`)
+    console.log(`Server is now listening on the Port ${port}`)
 });
